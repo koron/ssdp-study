604bd45f6c0dfc14e97248824a3ff2f5a47edbce
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-05-20 07:59:38 +0900

    format output

--- listif.go
+++ listif.go
@@ -11,7 +11,20 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to obtain interfaces: %s", err)
 	}
-	for i, n := range ini {
-		fmt.Printf("%d: %#v\n", i, n)
+	for i, ifi := range ini {
+		fmt.Printf("%d: %s\n", i, ifi.Name)
+		if addrs, err := ifi.Addrs(); err == nil {
+			fmt.Printf("  index: %d\n", ifi.Index)
+			fmt.Println("  addrs:")
+			for _, addr := range addrs {
+				fmt.Printf("    %s\n", addr.String())
+			}
+		}
+		if addrs, err := ifi.MulticastAddrs(); err == nil {
+			fmt.Println("  multicast-addrs:")
+			for _, addr := range addrs {
+				fmt.Printf("    %s\n", addr.String())
+			}
+		}
 	}
 }
